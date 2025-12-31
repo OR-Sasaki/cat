47087faf9fe6baeac875e4ce66ddd18bbaa25a23
letconst <67447234+letconst@users.noreply.github.com>
2026-01-01 02:16:08 +0900

    Add BackdropView with alpha control and click event

---
+++ Assets/Scripts/Root/Dialog/BackdropView.cs
@@ -0,0 +1,48 @@
+#nullable enable
+
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Root.Dialog
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    [RequireComponent(typeof(Image))]
+    public class BackdropView : MonoBehaviour, IPointerClickHandler
+    {
+        const float BaseAlpha = 0.5f;
+        const float AlphaIncrement = 0.1f;
+        const float MaxAlpha = 0.9f;
+
+        [SerializeField] CanvasGroup? _canvasGroup;
+
+        public event Action? OnClicked;
+
+        void Reset()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        void Awake()
+        {
+            _canvasGroup ??= GetComponent<CanvasGroup>();
+        }
+
+        public void SetAlphaByStackIndex(int stackIndex)
+        {
+            if (_canvasGroup == null)
+            {
+                return;
+            }
+
+            var alpha = BaseAlpha + (stackIndex * AlphaIncrement);
+            _canvasGroup.alpha = Mathf.Min(alpha, MaxAlpha);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            OnClicked?.Invoke();
+        }
+    }
+}

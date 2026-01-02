b310a4e07ba47ac753375607c027af3333c22cd9
letconst <67447234+letconst@users.noreply.github.com>
2026-01-03 02:04:28 +0900

    Add DialogCanvas prefab and DialogCanvasView

---
+++ Assets/Scripts/Root/View/DialogCanvasView.cs
@@ -0,0 +1,54 @@
+#nullable enable
+
+using Root.Service;
+using UnityEngine;
+using VContainer;
+
+namespace Root.View
+{
+    [RequireComponent(typeof(Canvas))]
+    public class DialogCanvasView : MonoBehaviour
+    {
+        [SerializeField] Canvas? _dialogCanvas;
+        [SerializeField] BackdropView? _backdropView;
+
+        DialogContainer? _dialogContainer;
+
+        [Inject]
+        public void Init(DialogContainer dialogContainer)
+        {
+            _dialogContainer = dialogContainer;
+        }
+
+        void Reset()
+        {
+            _dialogCanvas = GetComponent<Canvas>();
+        }
+
+        void Start()
+        {
+            if (_dialogContainer == null)
+            {
+                Debug.LogError("[DialogCanvasInitializer] DialogContainer is not injected.");
+                return;
+            }
+
+            if (_dialogCanvas == null)
+            {
+                Debug.LogError("[DialogCanvasInitializer] DialogCanvas is not assigned.");
+                return;
+            }
+
+            if (_backdropView == null)
+            {
+                Debug.LogError("[DialogCanvasInitializer] BackdropView is not assigned.");
+                return;
+            }
+
+            _dialogContainer.SetCanvas(_dialogCanvas);
+            _dialogContainer.SetBackdrop(_backdropView);
+
+            _backdropView.gameObject.SetActive(false);
+        }
+    }
+}

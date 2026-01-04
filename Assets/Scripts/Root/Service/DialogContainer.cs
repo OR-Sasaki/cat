5580f3abbb14750b3e7b516065434926b3a06424
letconst <67447234+letconst@users.noreply.github.com>
2026-01-04 19:16:28 +0900

    Add IObjectResolver to inject dependencies into dynamically instantiated dialogs

--- Assets/Scripts/Root/Service/DialogContainer.cs
+++ Assets/Scripts/Root/Service/DialogContainer.cs
@@ -17,6 +17,7 @@ namespace Root.Service
     public class DialogContainer : ITickable, IDisposable
     {
         readonly DialogState _dialogState;
+        readonly IObjectResolver _resolver;
         readonly Dictionary<string, AsyncOperationHandle<GameObject>> _prefabCache = new();
 
         Canvas? _dialogCanvas;
@@ -26,9 +27,10 @@ namespace Root.Service
         public event Action? OnBackButtonPressed;
 
         [Inject]
-        public DialogContainer(DialogState dialogState)
+        public DialogContainer(DialogState dialogState, IObjectResolver resolver)
         {
             _dialogState = dialogState;
+            _resolver = resolver;
         }
 
         public void SetCanvas(Canvas canvas)
@@ -59,6 +61,9 @@ namespace Root.Service
 
             var prefab = await LoadPrefabAsync(addressableKey, cancellationToken);
             var instance = UnityEngine.Object.Instantiate(prefab, _dialogCanvas.transform);
+
+            // Inject dependencies into dynamically instantiated dialog
+            _resolver.InjectGameObject(instance);
 
             var dialogView = instance.GetComponent<BaseDialogView>();
             if (dialogView == null)

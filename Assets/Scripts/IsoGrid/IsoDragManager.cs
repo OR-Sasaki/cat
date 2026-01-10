892d7092801d173b33ec6445821ffc88b197787d
OR-sasaki <eo29guramusu@gmail.com>
2026-01-10 20:11:09 +0900

    タッチでも家具を動かせるように

--- Assets/Scripts/IsoGrid/IsoDragManager.cs
+++ Assets/Scripts/IsoGrid/IsoDragManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 namespace Cat
 {
     /// <summary>
     /// Raycastベースのドラッグ管理システム
     /// 前面のColliderを貫通してIsoDraggableを検出する
+    /// PC: マウス入力、スマホ: タッチ入力に対応
     /// </summary>
     public class IsoDragManager : MonoBehaviour
     {
@@ -21,49 +23,85 @@ namespace Cat
 
         void Update()
         {
+            // タッチ入力を優先してチェック
+            if (Touch.activeTouches.Count > 0)
+            {
+                HandleTouchInput();
+            }
+            else
+            {
+                HandleMouseInput();
+            }
+        }
+
+        void HandleTouchInput()
+        {
+            var touch = Touch.activeTouches[0];
+            var worldPos = ScreenToWorldPosition(touch.screenPosition);
+
+            switch (touch.phase)
+            {
+                case UnityEngine.InputSystem.TouchPhase.Began:
+                    HandlePointerDown(worldPos);
+                    break;
+                case UnityEngine.InputSystem.TouchPhase.Moved:
+                case UnityEngine.InputSystem.TouchPhase.Stationary:
+                    if (_currentDraggable != null)
+                        HandlePointerDrag(worldPos);
+                    break;
+                case UnityEngine.InputSystem.TouchPhase.Ended:
+                case UnityEngine.InputSystem.TouchPhase.Canceled:
+                    if (_currentDraggable != null)
+                        HandlePointerUp();
+                    break;
+            }
+        }
+
+        void HandleMouseInput()
+        {
             var mouse = Mouse.current;
             if (mouse == null) return;
 
-            var mouseWorldPos = GetMouseWorldPosition(mouse);
+            var worldPos = ScreenToWorldPosition(mouse.position.ReadValue());
 
             if (mouse.leftButton.wasPressedThisFrame)
             {
-                HandleMouseDown(mouseWorldPos);
+                HandlePointerDown(worldPos);
             }
             else if (mouse.leftButton.isPressed && _currentDraggable != null)
             {
-                HandleMouseDrag(mouseWorldPos);
+                HandlePointerDrag(worldPos);
             }
             else if (mouse.leftButton.wasReleasedThisFrame && _currentDraggable != null)
             {
-                HandleMouseUp();
+                HandlePointerUp();
             }
         }
 
         /// <summary>
-        /// マウスダウン時の処理
+        /// ポインター押下時の処理
         /// </summary>
-        void HandleMouseDown(Vector3 mouseWorldPos)
+        void HandlePointerDown(Vector3 worldPos)
         {
-            var draggable = RaycastForDraggable(mouseWorldPos);
+            var draggable = RaycastForDraggable(worldPos);
             if (draggable == null) return;
 
             _currentDraggable = draggable;
-            _currentDraggable.BeginDrag(mouseWorldPos);
+            _currentDraggable.BeginDrag(worldPos);
         }
 
         /// <summary>
-        /// マウスドラッグ中の処理
+        /// ポインタードラッグ中の処理
         /// </summary>
-        void HandleMouseDrag(Vector3 mouseWorldPos)
+        void HandlePointerDrag(Vector3 worldPos)
         {
-            _currentDraggable.UpdateDrag(mouseWorldPos);
+            _currentDraggable.UpdateDrag(worldPos);
         }
 
         /// <summary>
-        /// マウスアップ時の処理
+        /// ポインター離した時の処理
         /// </summary>
-        void HandleMouseUp()
+        void HandlePointerUp()
         {
             _currentDraggable.EndDrag();
             _currentDraggable = null;
@@ -97,13 +135,12 @@ namespace Cat
         }
 
         /// <summary>
-        /// マウスのワールド座標を取得
+        /// スクリーン座標をワールド座標に変換
         /// </summary>
-        Vector3 GetMouseWorldPosition(Mouse mouse)
+        Vector3 ScreenToWorldPosition(Vector2 screenPos)
         {
-            var mousePos = mouse.position.ReadValue();
-            var screenPos = new Vector3(mousePos.x, mousePos.y, -_mainCamera.transform.position.z);
-            return _mainCamera.ScreenToWorldPoint(screenPos);
+            var pos = new Vector3(screenPos.x, screenPos.y, -_mainCamera.transform.position.z);
+            return _mainCamera.ScreenToWorldPoint(pos);
         }
     }
 }

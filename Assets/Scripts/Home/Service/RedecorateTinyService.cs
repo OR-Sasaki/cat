2fd69a7a26f0fc8eab48814a23d21f79d3db4a82
OR-sasaki <eo29guramusu@gmail.com>
2026-01-25 16:59:27 +0900

    責務明確化

--- Assets/Scripts/Home/Service/RedecorateTinyService.cs
+++ Assets/Scripts/Home/Service/RedecorateTinyService.cs
@@ -1,5 +1,4 @@
 using Home.View;
-using UnityEngine;
 using VContainer.Unity;
 
 namespace Home.Service
@@ -7,13 +6,7 @@ namespace Home.Service
     /// UIを画面下部へ縮小するTiny機能を管理するサービス
     public class RedecorateTinyService : IStartable
     {
-        static readonly int TinyParam = Animator.StringToHash("Tiny");
-
         readonly RedecorateUiView _redecorateUiView;
-
-        bool _isTiny;
-
-        public bool IsTiny => _isTiny;
 
         public RedecorateTinyService(RedecorateUiView redecorateUiView)
         {
@@ -23,27 +16,19 @@ namespace Home.Service
         public void Start()
         {
             _redecorateUiView.TinyButton.onClick.AddListener(ToggleTiny);
-            _redecorateUiView.OnOpen.AddListener(ResetTiny);
+            _redecorateUiView.OnOpen.AddListener(_redecorateUiView.ResetTiny);
         }
 
         /// Tiny状態をトグルする
         public void ToggleTiny()
         {
-            SetTiny(!_isTiny);
+            _redecorateUiView.SetTiny(!_redecorateUiView.IsTiny);
         }
 
         /// Tiny状態を設定する
         public void SetTiny(bool isTiny)
         {
-            _isTiny = isTiny;
-            _redecorateUiView.TinyAnimator.SetBool(TinyParam, _isTiny);
-        }
-
-        /// Tiny状態をリセットする（画面を開いた時に呼ばれる）
-        void ResetTiny()
-        {
-            _isTiny = false;
-            _redecorateUiView.TinyAnimator.SetBool(TinyParam, false);
+            _redecorateUiView.SetTiny(isTiny);
         }
     }
 }
--- Assets/Scripts/Home/View/RedecorateUiView.cs
+++ Assets/Scripts/Home/View/RedecorateUiView.cs
@@ -20,14 +20,31 @@ namespace Home.View
         [SerializeField] Animator _tinyAnimator;
         [SerializeField] Button _tinyButton;
 
+        static readonly int TinyParam = Animator.StringToHash("Tiny");
+
+        bool _isTiny;
+
+        public bool IsTiny => _isTiny;
+
         public EnhancedScroller Scroller => _scroller;
         public EnhancedScrollerCellView CellViewPrefab => _cellViewPrefab;
         public int NumberOfCellsPerRow => _numberOfCellsPerRow;
         public float CellViewSize => _cellViewSize;
         public float BottomPadding => _bottomPadding;
 
-        public Animator TinyAnimator => _tinyAnimator;
         public Button TinyButton => _tinyButton;
+
+        public void SetTiny(bool isTiny)
+        {
+            _isTiny = isTiny;
+            _tinyAnimator.SetBool(TinyParam, _isTiny);
+        }
+
+        public void ResetTiny()
+        {
+            _isTiny = false;
+            _tinyAnimator.SetBool(TinyParam, false);
+        }
 
         [Inject]
         public void Init(HomeStateSetService homeStateSetService)
